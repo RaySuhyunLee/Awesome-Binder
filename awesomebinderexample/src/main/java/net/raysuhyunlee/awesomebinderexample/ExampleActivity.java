52323df38a9f15182efe3278f7421bb66a66f834
이수현 <SuhyunLee@YasuBook.local>
2015-11-30 02:29:09 +0900

    Test function binding

--- awesomebinderexample/src/main/java/net/raysuhyunlee/awesomebinderexample/ExampleActivity.java
+++ awesomebinderexample/src/main/java/net/raysuhyunlee/awesomebinderexample/ExampleActivity.java
@@ -9,10 +9,19 @@ import net.raysuhyunlee.awesomebinder.AwesomeBinder;
  * Created by SuhyunLee on 2015. 11. 19..
  */
 public class ExampleActivity extends Activity {
+    AwesomeBinder binder;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AwesomeBinder binder = new AwesomeBinder();
+        binder = new AwesomeBinder();
         binder.setContentView(this, R.layout.activity_example);
+    }
+
+    public String isValid() {
+        if (binder.getValue("hello").equals("hello")) {
+            return "true";
+        } else {
+            return "false";
+        }
     }
 }
